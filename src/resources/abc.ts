25dfc884647698aeca75ed7cc563c93c2a689129
i0bs <41456914+i0bs@users.noreply.github.com>
2022-11-23 04:06:14 -0500

    Fix indentation level for Snowflake

--- src/resources/abc.ts
+++ src/resources/abc.ts
@@ -1,17 +1,17 @@
 export class Snowflake {
-    readonly timestamp: number
-    readonly workerId: number
-    readonly processId: number
-    readonly increment: number
+  readonly timestamp: number
+  readonly workerId: number
+  readonly processId: number
+  readonly increment: number
 
-    constructor(public value: number) {
-      this.timestamp = (this.value >> 22) + 1420070400000
-      this.workerId = (this.value & 0x3E0000) >> 17
-      this.processId = (this.value & 0x1F000) >> 12
-      this.increment = this.value & 0xFFF
-    }
+  constructor(public value: number) {
+    this.timestamp = (this.value >> 22) + 1420070400000
+    this.workerId = (this.value & 0x3E0000) >> 17
+    this.processId = (this.value & 0x1F000) >> 12
+    this.increment = this.value & 0xFFF
+  }
 
-    toDate(): Date {
-        return new Date(this.timestamp)
-    }
+  toDate(): Date {
+    return new Date(this.timestamp)
+  }
 }
