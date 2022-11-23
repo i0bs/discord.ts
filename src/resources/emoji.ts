dee13c681cab53a0736697e2910e90105b9f8aaf
i0bs <41456914+i0bs@users.noreply.github.com>
2022-11-23 15:11:23 -0500

    Add mappings for Emoji props

--- src/resources/emoji.ts
+++ src/resources/emoji.ts
@@ -2,14 +2,19 @@ import { Snowflake } from "./abc"
 import { Serializable, Mapped } from "../utils/serializer"
 
 export class Emoji extends Serializable {
+  @Mapped
   id: Snowflake | null
+  @Mapped
   name: string | null
 //  roles?: Role[]
 //  user?: User
   @Mapped("require_colons")
   requireColons?: boolean
+  @Mapped
   managed?: boolean
+  @Mapped
   animated?: boolean
+  @Mapped
   available?: boolean
 
   constructor() { super() }
